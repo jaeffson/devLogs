f0c332177e086e2995e18acfd2ae630a8846de3b
Jaeffson Sabino <jaeffsonsabino@hotmail.com>
2025-10-19 22:08:34 -0300

    mudancavie.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import React from 'react'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [React(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
