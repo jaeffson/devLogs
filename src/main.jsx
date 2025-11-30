97b192adb663ac8059beb3078ad70de9858e6b3c
Jaeffson Sabino <jaeffsonsabino@hotmail.com>
2025-11-30 16:19:10 -0300

    oo

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// AS IMPORTAÇÕES ABAIXO FORAM REMOVIDAS:
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.jsx';
 import './index.css';
 import { Toaster } from 'react-hot-toast'; 
-
-// A inicialização do QueryClient TAMBÉM FOI REMOVIDA:
-// const queryClient = new QueryClient({...}); 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
